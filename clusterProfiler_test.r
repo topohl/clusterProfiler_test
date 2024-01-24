f54913ea610a09f1e1f5b50e2e2a62fb88f45352
Tobias Pohl <119701302+topohl@users.noreply.github.com>
2024-01-24 09:43:20 +0100

    Update clusterProfiler_test.r

--- clusterProfiler_test.r
+++ clusterProfiler_test.r
@@ -9,25 +9,22 @@ BiocManager::install("enrichplot")
 
 
 # Install and load required packages
-```{r, message=F, warning=F}
+
 library(clusterProfiler)
 library(enrichplot)
 # we use ggplot2 to add x axis labels (ex: ridgeplot)
 library(ggplot2)
-```
+
 
 # Annotations
 # I'm using *D melanogaster* data, so I install and load the annotation "org.Dm.eg.db" below. See all annotations available here: http://bioconductor.org/packages/release/BiocViews.html#___OrgDb (there are 19 presently available). 
 
-```{r, message=F, warning=F}
 # SET THE DESIRED ORGANISM HERE
 organism <- "org.Mm.eg.db"
 BiocManager::install(organism, character.only = TRUE)
 library(organism, character.only = TRUE)
-```
 
 #Prepare Input
-```{r}
 # reading in data from deseq2
 df <- read.csv("C:/Users/topohl/Documents/Data/neuropilvsmicroglia_log2fc.csv", header = TRUE)
 # Rename the first column
@@ -64,7 +61,7 @@ gene_list <- sort(gene_list, decreasing = TRUE)
 # **pvalueCutoff** pvalue Cutoff.   
 # **pAdjustMethod** one of "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none" 
 
-```{r}
+
 library(clusterProfiler)
 
 gse <- gseGO(geneList = gene_list,
@@ -77,66 +74,62 @@ gse <- gseGO(geneList = gene_list,
              verbose = TRUE,
              OrgDb = organism,
              pAdjustMethod = "none")
-```
 
 # Output
 ##Table of results
-```{r}
+
 head(gse)
-```
 
 ##Dotplot
-```{r echo=TRUE, fig.width = 15, fig.height=8}
+
 require(DOSE)
 dotplot(gse, showCategory = 10, split = ".sign") + facet_grid(. ~ .sign)
-```
+
 
 ##Encrichment plot map:
 #Enrichment map organizes enriched terms into a network with edges connecting overlapping gene sets. In this way, mutually overlapping gene sets are tend to cluster together, making it easy to identify functional modules.
-```{r echo = TRUE}
+
 library(ggplot2)
 install.packages("ggnewscale")
 library(ggnewscale)
 similarity_matrix <- pairwise_termsim(gse)
 emapplot(similarity_matrix, showCategory = 10)
-# emapplot(gse, showCategory = 10)
+#emapplot(gse, showCategory = 10)
 
-```
 
 ##Category Netplot
-#The cnetplot depicts the linkages of genes and biological concepts (e.g. GO terms or KEGG pathways) as a network (helpful to see which genes are involved in enriched pathways and genes that may belong to multiple annotation categories).
-```{r fig.width = 18}
+#The cnetplot depicts the linkages of genes and biological concepts (e.g. GO terms or KEGG pathwayhttp://127.0.0.1:39791/graphics/88bf7a0b-6d09-4b33-907e-5bddff6adaa1.pngs) as a network (helpful to see which genes are involved in enriched pathways and genes that may belong to multiple annotation categories).
+
 # categorySize can be either 'pvalue' or 'geneNum'
 cnetplot(gse, categorySize = "pvalue", foldChange = gene_list, showCategory = 3)
-```
+
 
 ## Ridgeplot
-Helpful to interpret up/down-regulated pathways.
-```{r fig.width = 18, fig.height = 12}
+# Helpful to interpret up/down-regulated pathways.
+
 install.packages("ggridges")
 library(ggridges)
 ridgeplot(gse) + labs(x = "enrichment distribution")
-```
+
 
 ## GSEA Plot  
-Traditional method for visualizing GSEA result.  
+#Traditional method for visualizing GSEA result.  
   
-Params:  
-**Gene Set** Integer. Corresponds to gene set in the gse object. The first gene set is 1, second gene set is 2, etc. 
+#Params:  
+#**Gene Set** Integer. Corresponds to gene set in the gse object. The first gene set is 1, second gene set is 2, etc. 
 
-```{r fig.height=6}
+
 # Use the `Gene Set` param for the index in the title, and as the value for geneSetId
 gseaplot(gse, by = "all", title = gse$Description[1], geneSetID = 1)
-```
+
 
 ## PubMed trend of enriched terms
-Plots the number/proportion of publications trend based on the query result from PubMed Central.
-```{r fig.width = 10}
+#Plots the number/proportion of publications trend based on the query result from PubMed Central.
+
 install.packages("europepmc")
 library(europepmc)
 terms <- gse$Description[1:3]
 pmcplot(terms, 2010:2018, proportion = FALSE)
-```
 
 
 # KEGG Gene Set Enrichment Analysis
@@ -149,7 +142,7 @@ pmcplot(terms, 2010:2018, proportion = FALSE)
 # As our intial input, we use `original_gene_list` which we created above.
 
 ## Prepare Input
-```{r}
+
 # Convert gene IDs for gseKEGG function
 # We will lose some genes here because not all IDs will be converted
 ids<-bitr(names(original_gene_list), fromType = "SYMBOL", toType = "UNIPROT", OrgDb=organism)
@@ -175,17 +168,17 @@ kegg_gene_list<-na.omit(kegg_gene_list)
 # sort the list in decreasing order (required for clusterProfiler)
 kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
 
-```
+
 ## Create gseKEGG object
  
-**organism** KEGG Organism Code: The full list is here: https://www.genome.jp/kegg/catalog/org_list.html (need the 3 letter code). I define this as `kegg_organism` first, because it is used again below when making the pathview plots.  
-**nPerm** permutation numbers, the higher the number of permutations you set, the more accurate your results is, but it will also cost longer time for running permutation.  
-**minGSSize** minimal size of each geneSet for analyzing.   
-**maxGSSize** maximal size of genes annotated for testing.   
-**pvalueCutoff** pvalue Cutoff.   
-**pAdjustMethod** one of "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none".  
-**keyType** one of 'kegg', 'ncbi-geneid', 'ncib-proteinid' or 'uniprot'.  
-```{r}
+#**organism** KEGG Organism Code: The full list is here: https://www.genome.jp/kegg/catalog/org_list.html (need the 3 letter code). I define this as `kegg_organism` first, because it is used again below when making the pathview plots.  
+#**nPerm** permutation numbers, the higher the number of permutations you set, the more accurate your results is, but it will also cost longer time for running permutation.  
+#**minGSSize** minimal size of each geneSet for analyzing.   
+#**maxGSSize** maximal size of genes annotated for testing.   
+#**pvalueCutoff** pvalue Cutoff.   
+#**pAdjustMethod** one of "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none".  
+#**keyType** one of 'kegg', 'ncbi-geneid', 'ncib-proteinid' or 'uniprot'.  
+
 
 library(clusterProfiler)
 getOption("clusterProfiler.download.method")
@@ -204,56 +197,55 @@ kk2 <- gseKEGG(geneList     = kegg_gene_list,
                pvalueCutoff = 0.05,
                pAdjustMethod = "none",
                keyType       = "uniprot")
-```
 
-```{r}
 head(kk2, 10)
-```
+
 
 ## Dotplot
-```{r echo=TRUE}
+
 dotplot(kk2, showCategory = 10, title = "Enriched Pathways" , split=".sign") + facet_grid(.~.sign)
-```
+
 
 ## Encrichment plot map:
-Enrichment map organizes enriched terms into a network with edges connecting overlapping gene sets. In this way, mutually overlapping gene sets are tend to cluster together, making it easy to identify functional modules.
-```{r echo=TRUE}
- emapplot(kk2)
-```
+#Enrichment map organizes enriched terms into a network with edges connecting overlapping gene sets. In this way, mutually overlapping gene sets are tend to cluster together, making it easy to identify functional modules.
+
+emapplot(kk2)
+
 
 ## Category Netplot:
-The cnetplot depicts the linkages of genes and biological concepts (e.g. GO terms or KEGG pathways) as a network (helpful to see which genes are involved in enriched pathways and genes that may belong to multiple annotation categories).
-```{r fig.width=12}
+#The cnetplot depicts the linkages of genes and biological concepts (e.g. GO terms or KEGG pathways) as a network (helpful to see which genes are involved in enriched pathways and genes that may belong to multiple annotation categories).
+
 # categorySize can be either 'pvalue' or 'geneNum'
 cnetplot(kk2, categorySize="pvalue", foldChange=gene_list)
-```
+
 
 ## Ridgeplot
-Helpful to interpret up/down-regulated pathways.
-```{r fig.width=18, fig.height=12}
+#Helpful to interpret up/down-regulated pathways.
+
 ridgeplot(kk2) + labs(x = "enrichment distribution")
-```
+
 
 # GSEA Plot  
-Traditional method for visualizing GSEA result.  
+#Traditional method for visualizing GSEA result.  
   
-Params:  
-**Gene Set** Integer. Corresponds to gene set in the gse object. The first gene set is 1, second gene set is 2, etc. Default: 1  
+#Params:  
+#**Gene Set** Integer. Corresponds to gene set in the gse object. The first gene set is 1, second gene set is 2, etc. Default: 1  
 
-```{r fig.height=6}
+
 # Use the `Gene Set` param for the index in the title, and as the value for geneSetId
 gseaplot(kk2, by = "all", title = kk2$Description[1], geneSetID = 1)
-```
+
 
 #Pathview
-This will create a PNG and *different* PDF of the enriched KEGG pathway.  
+#This will create a PNG and *different* PDF of the enriched KEGG pathway.  
   
-Params:  
-**gene.data** This is `kegg_gene_list` created above  
-**pathway.id** The user needs to enter this. Enriched pathways + the pathway ID are provided in the gseKEGG output table (above).  
-**species** Same as `organism` above in `gseKEGG`, which we defined as `kegg_organism`
+#Params:  
+#**gene.data** This is `kegg_gene_list` created above  
+#**pathway.id** The user needs to enter this. Enriched pathways + the pathway ID are provided in the gseKEGG output table (above).  
+#**species** Same as `organism` above in `gseKEGG`, which we defined as `kegg_organism`
 
-```{r, message=F, warning=F, echo = TRUE}
+
+install.packages("pathview")
 library(pathview)
 
 # Produce the native KEGG plot (PNG)
@@ -261,7 +253,6 @@ dme <- pathview(gene.data=kegg_gene_list, pathway.id="dme04130", species = kegg_
 
 # Produce a different plot (PDF) (not displayed here)
 dme <- pathview(gene.data=kegg_gene_list, pathway.id="dme04130", species = kegg_organism, kegg.native = F)
-```
-```{r pressure, echo=TRUE, fig.cap="KEGG Native Enriched Pathway Plot", out.width = '100%'}
+
+#```{r pressure, echo=TRUE, fig.cap="KEGG Native Enriched Pathway Plot", out.width = '100%'}
 knitr::include_graphics("dme04130.pathview.png")
-```
