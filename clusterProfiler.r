fe4732478db62761222b6a50d0faec0615361f14
Tobias Pohl <119701302+topohl@users.noreply.github.com>
2025-04-14 17:17:59 +0200

    Update clusterProfiler.r
    + added roxygen style documentation
    + minor modifications

--- clusterProfiler.r
+++ clusterProfiler.r
@@ -1,40 +1,63 @@
-# Introduction: Gene Set Enrichment Analysis (GSEA) using clusterProfiler
-# Documentation: https://bioconductor.org/packages/release/bioc/vignettes/clusterProfiler/inst/doc/clusterProfiler.html
+#' Gene Set Enrichment Analysis (GSEA) Workflow using clusterProfiler
+#'
+#' This script performs Gene Set Enrichment Analysis (GSEA), KEGG pathway enrichment,
+#' and visualization of functional gene sets using the `clusterProfiler` ecosystem.
+#' It supports mouse datasets and creates various publication-quality plots.
+#'
+#' @details
+#' The analysis includes:
+#' - Setup of required packages and environment
+#' - Loading and sorting of gene lists based on log2 fold change
+#' - Gene Ontology (GO) GSEA
+#' - KEGG GSEA (symbol to ENTREZID conversion)
+#' - Functional plots: dotplots, network plots, ridgeplots, GSEA plots
+#' - PubMed trend analysis for top terms
+#' - GO enrichment analysis and heatmaps
+#'
+#' @references
+#' clusterProfiler vignette: \url{https://bioconductor.org/packages/release/bioc/vignettes/clusterProfiler/inst/doc/clusterProfiler.html}
+#'
+#' @author
+#' Tobias Pohl
+
+# ----------------------------------------------------
+# Install and load packages
+# ----------------------------------------------------
 
 # Check and install required packages
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
+  install.packages("BiocManager")
 }
 
 # Define required packages
 required_packages <- c(
-    "clusterProfiler", "pathview", "enrichplot", "DOSE", "ggplot2", "ggnewscale",
-    "cowplot", "ggridges", "europepmc", "ggpubr", "ggrepel", "ggsci", "ggthemes",
-    "ggExtra", "ggforce", "ggalluvial", "lattice", "latticeExtra", "BiocManager",
-    "org.Mm.eg.db", "ggplotify", "svglite"
+  "clusterProfiler", "pathview", "enrichplot", "DOSE", "ggplot2", "ggnewscale",
+  "cowplot", "ggridges", "europepmc", "ggpubr", "ggrepel", "ggsci", "ggthemes",
+  "ggExtra", "ggforce", "ggalluvial", "lattice", "latticeExtra", "BiocManager",
+  "org.Mm.eg.db", "ggplotify", "svglite"
 )
 
 # Install missing Bioconductor packages
-bioc_packages <- c("clusterProfiler", "pathview", "enrichplot", "DOSE", "org.Mm.eg.db")
+bioc_packages <- c(
+  "clusterProfiler", "pathview", "enrichplot", "DOSE", "org.Mm.eg.db"
+)
+
 missing_bioc <- bioc_packages[!sapply(bioc_packages, requireNamespace, quietly = TRUE)]
 if (length(missing_bioc) > 0) {
-    BiocManager::install(missing_bioc)
+  BiocManager::install(missing_bioc)
 }
 
-# Install and load all required packages
 install_and_load <- function(package) {
-    if (!require(package, character.only = TRUE)) {
-        install.packages(package, dependencies = TRUE)
-    }
-    library(package, character.only = TRUE)
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+  }
+  library(package, character.only = TRUE)
 }
-
 invisible(lapply(required_packages, install_and_load))
 
-# Set annotation package and load it
-organism <- "org.Mm.eg.db" 
-BiocManager::install(organism, character.only = TRUE)
-library(organism, character.only = TRUE)
+# ----------------------------------------------------
+# Define working directory, cell types, and data paths
+# ----------------------------------------------------
 
 # Set directories
 working_dir <- "S:/Lab_Member/Tobi/Experiments/Exp3_Nlgn3_development/LaserDissProteomics/GSEA"
@@ -46,12 +69,26 @@ cell_types <- c("neuropil", "microglia")
 file_name <- paste0(paste(cell_types, collapse = "_"), ".csv")
 data_path <- file.path(working_dir, "Datasets", file_name)
 
+# ----------------------------------------------------
+# Load and prepare data
+# ----------------------------------------------------
+
 # Load and prepare gene data
 df <- read.csv(data_path, header = TRUE)
 colnames(df)[1] <- "gene_symbol"
+
 original_gene_list <- df$log2fc
 names(original_gene_list) <- df$gene_symbol
 gene_list <- sort(na.omit(original_gene_list), decreasing = TRUE)
+
+# ----------------------------------------------------
+# Perform Gene Set Enrichment Analysis (GSEA)
+# ----------------------------------------------------
+
+# Set annotation package and load it
+organism <- "org.Mm.eg.db" 
+BiocManager::install(organism, character.only = TRUE)
+library(organism, character.only = TRUE)
 
 # Gene Set Enrichment Analysis (GSEA)
 gse <- gseGO(
@@ -60,7 +97,7 @@ gse <- gseGO(
   OrgDb = organism, pAdjustMethod = "none"
 )
 
-# Plotting function for reuse
+# Helper to save plots
 save_plot <- function(plot, filename) {
   ggsave(file.path(results_dir, filename), plot, units = "cm", dpi = 300)
 }
@@ -72,6 +109,10 @@ p1 <- clusterProfiler::dotplot(gse, showCategory = 10, split = ".sign") +
   facet_grid(. ~ .sign) +
   labs(title = dot_title)
 save_plot(p1, paste("GSEAdotplot_", paste(cell_types, collapse = "_"), ".svg"))
+
+# ----------------------------------------------------
+# Enrichment Map, Network Plot, and Ridgeplot of GSEA
+# ----------------------------------------------------
 
 # Enrichment Map and Network Plot
 emapplot(pairwise_termsim(gse), showCategory = 10)
@@ -96,8 +137,12 @@ save_plot(gseaplot_gse, paste("GSEA_Plot_", paste(cell_types, collapse = "_"), "
 # PubMed Trend Plot: Analyzing publication trends for top enriched terms
 top_terms <- head(gse@result$Description, 3)
 pmcplot_gse <- pmcplot(top_terms, 2010:2018, proportion = FALSE) +
-    labs(title = "Publication Trends for Top Enriched Terms")
+  labs(title = "Publication Trends for Top Enriched Terms")
 save_plot(pmcplot_gse, paste("GSEA_PubMed_Trends_", paste(cell_types, collapse = "_"), ".svg"))
+
+# ----------------------------------------------------
+# KEGG Gene Set Enrichment Analysis
+# ----------------------------------------------------
 
 # KEGG GSEA Analysis
 # Map SYMBOL to ENTREZID
@@ -118,15 +163,15 @@ kegg_gene_list <- sort(na.omit(kegg_gene_list), decreasing = TRUE)
 
 kegg_organism <- "mmu"
 kk2 <- gseKEGG(
-    geneList = kegg_gene_list, organism = "mmu",
-    minGSSize = 3, maxGSSize = 800, pvalueCutoff = 1, pAdjustMethod = "none",
-    keyType = "ncbi-geneid"
+  geneList = kegg_gene_list, organism = "mmu",
+  minGSSize = 3, maxGSSize = 800, pvalueCutoff = 1, pAdjustMethod = "none",
+  keyType = "ncbi-geneid"
 )
 
 # KEGG Dotplot and Network Plot
 kegg_dot_title <- paste("KEGG GSEA Enriched Pathways of", paste(cell_types, collapse = " over "))
 p2 <- clusterProfiler::dotplot(kk2, showCategory = 10, title = kegg_dot_title, split = ".sign") +
-    facet_grid(. ~ .sign)
+  facet_grid(. ~ .sign)
 save_plot(p2, paste("KEGGpathway_", paste(cell_types, collapse = "_"), ".svg"))
 
 emapplot(pairwise_termsim(kk2), showCategory = 10)
@@ -141,17 +186,18 @@ library(pathview)
 pathview(gene.data = kegg_gene_list, pathway.id = "mmu00030", species = kegg_organism)
 pathview(gene.data = kegg_gene_list, pathway.id = "mmu00030", species = kegg_organism, kegg.native = FALSE)
 
+# ----------------------------------------------------
+# EnrichGO Analysis and Heatmap
+# ----------------------------------------------------
+
 # EnrichGO Analysis and Heatmap
 go_enrich <- enrichGO(
-    gene = names(gene_list), universe = names(gene_list), OrgDb = organism, 
-    keyType = 'SYMBOL', readable = TRUE, ont = "ALL", pvalueCutoff = 1, 
-    qvalueCutoff = 1, pAdjustMethod = "none", minGSSize = 3, maxGSSize = 800
+  gene = names(gene_list), universe = names(gene_list), OrgDb = organism, 
+  keyType = 'SYMBOL', readable = TRUE, ont = "ALL", pvalueCutoff = 1, 
+  qvalueCutoff = 1, pAdjustMethod = "none", minGSSize = 3, maxGSSize = 800
 )
 
 # Heatmap Plot
 p3 <- heatplot(go_enrich, foldChange = gene_list, showCategory = 5)
-p3 <- p3 + labs(title = paste("GO Enrichment Heatmap of", paste(cell_types, collapse = " over ")))
-save_plot(p3, paste("GOheatmap_", paste(cell_types, collapse = "_"), ".svg"))
-
 save_plot(p4, paste("GOheatmap_", paste(cell_types, collapse = "_"), ".svg"))
 cowplot::plot_grid(p1, p3, ncol = 1, labels = LETTERS[1:2])
